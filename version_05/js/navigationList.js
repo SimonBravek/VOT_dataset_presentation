c881d9c4dd275c842fbe3dc18b76da66e6d233ab
SimonBravek <138696200+SimonBravek@users.noreply.github.com>
2023-08-16 20:57:41 +0200

    Add files via upload

---
+++ version_05/js/navigationList.js
@@ -0,0 +1,63 @@
+// This creates the video name list that sends u to the right video after clicking and the downloading in the side
+
+import { videoNameList } from './01_inputUpdateNeeded.js';
+
+// Function to capitalize the first letter, remove numbers, and replace underscores with spaces and Create the list more readable
+function formatItem(item) {
+  // Special case for "F1"
+  if (item === "f1") {
+    return "F1";
+  }
+  
+  return item.charAt(0).toUpperCase() + item.slice(1)
+    .replace(/([0-9]+)$/, ' $1')
+    .replace(/_/g, ' ')
+    .replace(/-/g, '');
+}
+
+// Get the unordered list element
+const myList = document.getElementById("myList");
+// Loop through the items list and create the list elements
+for (const item of videoNameList) {
+  // Create the list item and anchor elements
+  const listItem = document.createElement("li");
+  const anchorElement1 = document.createElement("a");
+  const anchorElement2 = document.createElement("a");
+  const svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const pathElement = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  
+  // Set the href attributes for the anchor elements
+  anchorElement1.setAttribute("href", "#" + item);
+  anchorElement2.setAttribute("href", "Media/" + item + "/" + item + ".mp4"); // HIGHLIGHT: there is a part of the folder structure
+  anchorElement2.setAttribute("download", "");
+  
+  // Add class for the second anchor element
+  anchorElement2.classList.add("hovering");
+  
+  // Set the class attribute for the SVG element
+  svgElement.setAttribute("class", "hoverElement");
+  svgElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  svgElement.setAttribute("viewBox", "0 0 24 24");
+  
+  // Set the 'd' attribute for the path element
+  pathElement.setAttribute("d", "M12 16l-4-4h3V4h2v8h3l-4 4zm-4 5h8v2H8z");
+  
+  // Create the text node for the first anchor element
+  const textNode = document.createTextNode(formatItem(item));
+  
+  // Append the text node to the first anchor element
+  anchorElement1.appendChild(textNode);
+  
+  // Append the path element to the SVG element
+  svgElement.appendChild(pathElement);
+  
+  // Append the SVG element to the second anchor element
+  anchorElement2.appendChild(svgElement);
+  
+  // Append the anchor elements to the list item
+  listItem.appendChild(anchorElement1);
+  listItem.appendChild(anchorElement2);
+  
+  // Append the list item to the unordered list
+  myList.appendChild(listItem);
+}
---
+++ version_05/js/settingsDevelopment.js
@@ -0,0 +1,72 @@
+// Creates the bigger video with description after clicking the button
+
+import {anchorList, videoPathList, innitBoxPathList, innitMaskList, videoNameList} from './01_inputUpdateNeeded.js'
+
+export function createElementNS(namespaceURI, qualifiedName) {
+    return document.createElementNS(namespaceURI, qualifiedName);
+}
+
+export function setAttribute(element, attribute, value) {
+    element.setAttribute(attribute, value);
+}
+
+export function appendChild(parent, child) {
+    parent.appendChild(child);
+}
+
+export function createSettingsElement(i) {
+    // Create the settings-bg element
+    const settingsBg = createElementNS("http://www.w3.org/1999/xhtml", "div");
+    let currentClass = "settings-bg settings " + videoNameList[i]
+    setAttribute(settingsBg, "class", currentClass);
+
+    // Create the settings-container element
+    const settingsContainer = createElementNS("http://www.w3.org/1999/xhtml", "div");
+    currentClass = "settings-container settings " + videoNameList[i]
+    setAttribute(settingsContainer, "class", currentClass);
+    appendChild(settingsBg, settingsContainer);
+
+    // Create the video-sequence element inside the settings-container
+    const videoSequenceSettings = createElementNS("http://www.w3.org/1999/xhtml", "video");
+    currentClass = "video-sequence settings " + videoNameList[i]
+    setAttribute(videoSequenceSettings, "class", currentClass);
+    setAttribute(videoSequenceSettings, "src", videoPathList[i]);
+    setAttribute(videoSequenceSettings, "type", "video/mp4");
+    setAttribute(videoSequenceSettings, "controls", "");
+    setAttribute(videoSequenceSettings, "muted", "");
+    setAttribute(videoSequenceSettings, "autoplay", "");
+    appendChild(settingsContainer, videoSequenceSettings);
+
+    // Create the h2 element inside the settings-container
+    const h2Settings = createElementNS("http://www.w3.org/1999/xhtml", "h2");
+    currentClass = "settings " + videoNameList[i]
+    setAttribute(h2Settings, "class", currentClass);
+    h2Settings.textContent = videoNameList[i];
+    appendChild(settingsContainer, h2Settings);
+
+    // Create the text element inside the settings-container
+    const textSettings = createElementNS("http://www.w3.org/1999/xhtml", "div");
+    currentClass = "text settings " + videoNameList[i]
+    setAttribute(textSettings, "class", currentClass);
+    appendChild(settingsContainer, textSettings);
+
+    const arrayOfArrays = [videoPathList, innitBoxPathList, innitMaskList];
+
+    for (const item of arrayOfArrays) {
+        const a = createElementNS("http://www.w3.org/1999/xhtml", "a");
+        currentClass = "anchor settings " + videoNameList[i]
+        setAttribute(a, "class", currentClass);
+        setAttribute(a, "href", item[i]);
+        setAttribute(a, "download", "");
+        a.textContent = anchorList[arrayOfArrays.indexOf(item)];
+        appendChild(textSettings, a);
+    };
+
+    const p = createElementNS("http://www.w3.org/1999/xhtml", "p");
+    currentClass = "paragraph settings " + videoNameList[i];
+    p.textContent = anchorList[3];
+    appendChild(textSettings, p);
+
+    const videoBody = document.querySelector('.video-body');
+    appendChild(videoBody, settingsBg);
+};
