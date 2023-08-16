72d56841412fa1fb5c5e3218397bc0bc7b277bc5
SimonBravek <138696200+SimonBravek@users.noreply.github.com>
2023-08-16 12:48:16 +0200

    Delete NEWfolderWEBSITE2 directory

--- NEWfolderWEBSITE2/managePlayPause.js
+++
@@ -1,9 +0,0 @@
-export function managePlayPause() {
-    const settingsVideo = document.querySelector('.video-sequence.settings');
-    function handleKeyPress(event) {
-        if (event.keyCode === 75) {
-            settingsVideo.paused? settingsVideo.play(): settingsVideo.pause();
-        }
-    }
-    document.addEventListener('keydown', handleKeyPress);
-};
